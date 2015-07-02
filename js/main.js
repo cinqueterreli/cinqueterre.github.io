8ee69256155effd473d2339c60f2cf037da1e5d8
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-07-01 20:12:09 -0400

    ffff

--- js/main.js
+++ js/main.js
@@ -3,34 +3,14 @@ $(document).ready(function(){
 
 /*--- Dropdown Menu Functionality ---*/
 
-/* if(!!('ontouchstart' in window)){//check for touch device
-//behaviour and events for touch device
- $(".dropdown-link").click(function(){
-    $(".dropdown-menu").slideToggle(500);
-  });
-}
-
-else{ */
-  
-  $(".dropdown-link").mouseenter(function(){
-    $(".dropdown-menu").slideDown(500);
-  });
-
-  $(".dropdown-menu").mouseleave(function(){
-    $(".dropdown-menu").slideUp(200);
-  });
-
-  $('.no').mouseenter(function() {
-        $(".dropdown-menu").slideUp(200);
-  });
 
 
 /* ----- Box Shadow Changes on click ------- */
 
-  $(".category-image").mousedown(function(){
+  $(".button-link").mousedown(function(){
   	$(this).css("box-shadow", "none");
   });
-  $(".category-image").mouseup(function(){
+  $(".button-link").mouseup(function(){
     $(this).css("box-shadow", "3px 3px 3px 3px rgba(0, 0, 0, .5)");
   });
 
@@ -53,18 +33,7 @@ else{ */
 
 // ------ Media Queries ------ //
 
-  enquire.register("screen and (max-width:1025px)",{
-    match : function() {
-      $('#3').insertBefore("#2");
-      $('#5').insertBefore("#2");
-    },
-    unmatch : function() {
-      $('#5').insertBefore("#6");
-      $('#3').insertBefore("#4");
-      $('#2').insertAfter('#1');
-    }
 
-  });
 
 
 
