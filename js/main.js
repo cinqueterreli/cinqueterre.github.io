8aa985e377756e95ddbb2bcee67079db1c1f5ff0
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-07-08 21:02:36 -0400

    weee

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 /*--- Fixed position changer shit ---*/
 
 $(function() {  
-    var navTop = $('.menu-links').offset(200).top;  
+    var navTop = $('.menu-links').offset(400).top;  
 
     var stickyNav = function(){  
         var scrollTop = $(window).scrollTop();  
