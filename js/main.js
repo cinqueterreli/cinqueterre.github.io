3a9ecbb59191014565751d59f935675686de7fe7
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-07-08 21:51:58 -0400

    weee

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function() {
     var stickyNav = function(){  
         var scrollTop = $(window).scrollTop();  
 
-        if (scrollTop > navTop + 350) {   
+        if (scrollTop > navTop + 400) {   
             $('.sidebar').addClass('sticky');  
         } else {  
             $('.sidebar').removeClass('sticky');   
