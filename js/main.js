587bf4a95c9ef6650fdf8bbf3be73781631cfd04
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-07-08 20:19:53 -0400

    lets see what happens

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
 
 
+/*--- Fixed position changer shit ---*/
+
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('.sidebar').followTo(250);
+
+
 /*--- Dropdown Menu Functionality ---*/
 
 
