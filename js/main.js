04ebd386f65ce3e790e722c39f6e8088b6841d5d
cinqueterreli <Anthony@Anthonys-MBP.home>
2015-09-01 15:34:16 -0400

    weee

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ $('.location-link, .location-link-default').click(function(){
 
 
         $(window).scroll(function () {
-            if ($(this).scrollTop() > 400) {
+            if ($(this).scrollTop() > 600) {
                 $('#up-arrow').fadeIn();
             } else {
                 $('#up-arrow').fadeOut();
