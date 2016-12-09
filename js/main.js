e8cc2f3ec9fb5feea7470f1de2c7a2a88ed2cb06
cinqueterreli <Anthony@Anthonys-MBP.home>
2016-12-09 15:30:22 -0500

    YAY

--- js/main.js
+++ js/main.js
@@ -193,11 +193,11 @@ enquire.register("screen and (min-width:975px)",{
   });
 
 
-/* $('.promo-title').click(function(){
+$('.promo-title').click(function(){
   $(this).toggleClass('bottom-border');
   $(this).next().slideToggle(500);
   $(this).parent().toggleClass('full-height')
-}); */
+});
 
 });
 
